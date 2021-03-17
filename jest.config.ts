002317bfa639fc37ae3eb53397825f95de74f4ae
Nikos Kontakis <wirednkod@gmail.com>
2021-03-17 16:22:28 +0200

    test passed

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,7 @@
 export default {
   preset: "ts-jest",
   testEnvironment: 'node',
-  // transform: { "^.+\\.tsx?$": "ts-jest" },
+  transform: {},
   globals: {
     'ts-jest': {
       tsconfig: 'tsconfig.test.json'
